1b63a9ce14c1d6a4d85c4ef8d851f9f83b3934ff
“luhui1hao” <luhui1hao@gmail.com>
2018-09-19 15:53:27 +0800

    完成自定义View的练习

--- CustomView/practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ CustomView/practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +28,26 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(300, 165, 165, paint);
+
+        Paint paint2 = new Paint();
+        paint2.setColor(Color.BLACK);
+        paint2.setStyle(Paint.Style.STROKE);
+        paint2.setStrokeWidth(2);
+        paint2.setAntiAlias(true);//开启抗锯齿
+        canvas.drawCircle(700, 165, 165, paint2);
+
+        Paint paint3 = new Paint();
+        paint3.setColor(Color.BLUE);
+        canvas.drawCircle(300, 520, 165, paint3);
+
+        Paint paint4 = new Paint();
+        paint4.setColor(Color.BLACK);
+        paint4.setStyle(Paint.Style.STROKE);
+        paint4.setStrokeWidth(20);
+        canvas.drawCircle(700, 520, 165, paint4);
     }
 }
--- CustomView/practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ CustomView/practice1/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +27,16 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL); // 填充模式
+            //startAngle:从哪个角度开始，sweepAngle:画这么多角度
+            //逆时针为负数，正时针为正数
+            canvas.drawArc(200, 100, 800, 500, -110, 100, true, paint); // 绘制扇形
+            canvas.drawArc(200, 100, 800, 500, 20, 140, false, paint); // 绘制弧形
+            paint.setStyle(Paint.Style.STROKE); // 画线模式
+            canvas.drawArc(200, 100, 800, 500, 180, 60, false, paint); // 绘制不封口的弧形
+        }
     }
 }
